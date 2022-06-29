4cd5c3d2338713da289d1b059b16ab14646220cf
davidktlee <rudxor567@naver.com>
2022-06-29 14:41:16 +0900

    feat: Modify Not found page.

--- src/routes/NotFound.vue
+++ src/routes/NotFound.vue
@@ -1,6 +1,8 @@
 <template>
   <section>
-    <h1>404 Page Not Found!</h1>
+    <img src="https://t1.kakaocdn.net/estoreweb/images/20220421091219/empty_purchase.png" alt="" />
+    <h1>404 Page <br />Not Found!</h1>
+    <RouterLink to="/">홈으로 가기</RouterLink>
   </section>
 </template>
 
@@ -11,6 +13,19 @@ section {
   h1 {
     font-size: 60px;
     font-weight: 700;
+    line-height: 2;
+  }
+  a {
+    width: 120px;
+    padding: 10px 0;
+    margin: 10px auto;
+    text-align: center;
+    background-color: #eee;
+    color: #333;
+    border-radius: 10px;
+    &:hover {
+      background-color: darken(#eee, 10%);
+    }
   }
 }
 </style>
