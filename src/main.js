2cc4360a09e6a4d49f63e3f9ca5cbb7b66cf9f9b
YANGDAYOUNG <jc1525@hanmail.net>
2022-06-29 16:53:06 +0900

    feat: test

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
 import { createApp, markRaw } from 'vue'
 import App from '~/App.vue'
 import router from '~/routes'
-import './routes/guards'
+import '~/routes/guards'
 import { createPinia } from 'pinia'
-import request  from './plugins/request'
+import request  from '~/plugins/request'
 
 const pinia = createPinia()
 pinia.use(({ store }) => {
   store.$router = markRaw(router)
 })
 
-<<<<<<< HEAD
 createApp(App)
   .use(pinia)
   .use(router)
   .use(request)
   .mount('#app')
-=======
-createApp(App).use(pinia).use(router).mount('#app')
->>>>>>> main
