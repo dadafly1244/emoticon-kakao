f7c442747a27efae589ccd95fe4bfd500299a709
YANGDAYOUNG <jc1525@hanmail.net>
2022-06-29 11:20:00 +0900

    feat: add NotFound and PurchaseHistory in routes

---
+++ src/routes/PurchaseHistory.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>구매내역</h1>
+
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useUserStore } from '~/store/user'
+
+export default {
+  computed: {
+    ...mapStores(useUserStore),
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
