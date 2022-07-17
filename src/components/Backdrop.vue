95a946a42b488339c3e4ef440a20b3a238de6457
YANGDAYOUNG <jc1525@hanmail.net>
2022-07-17 21:58:18 +0900

    feat: add backdrop and purchase

---
+++ src/components/Backdrop.vue
@@ -0,0 +1,3 @@
+<template>
+<div class = "backdrop"></div>
+</template>
---
+++ src/components/ProductPruchase.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="modal">
+  <div class="modal-inner">
+    <div class="modal-head">
+      <div class="modal-title">구매하기</div>
+      <span class="material-symbols-rounded close-btn">close</span>
+    </div>
+    <div class="modal-body">
+      <div class="modal-body--inner">
+        <div class="box_order"></div>
+        <div class="box_order box_coupon"></div>
+        <div class="box_order box_agree"></div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
