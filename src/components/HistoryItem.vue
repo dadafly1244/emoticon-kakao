3f122559f8da1626a32ff255654d294d0b796695
davidktlee <rudxor567@naver.com>
2022-07-21 15:43:25 +0900

    feat: Styling historyItem component.

--- src/components/HistoryItem.vue
+++ src/components/HistoryItem.vue
@@ -1,62 +1,61 @@
 <template>
-  <div
-    class="container">
-    
+  <div class="container">
     <div class="product-img">
-        <img 
-          v-if="transaction.product.thumbnail"
-          :src="transaction.product.thumbnail" 
-          :alt="transaction.product.title"> 
-        <div v-else class="no-image"></div>
-      </div>
+      <img
+        v-if="transaction.product.thumbnail"
+        :src="transaction.product.thumbnail"
+        :alt="transaction.product.title"
+      />
+      <div v-else class="no-image"></div>
+    </div>
     <div class="product">
-      
       <div class="product-txt">
-        <div class="product-txt--id">제품ID : {{transaction.product.productId}}</div>
-        <div class="product-txt--title">제품이름 : {{transaction.product.title}}</div>
-        <div class="product-txt--price">제품가격 : {{transaction.product.price}}원</div>
-        <div class="product-txt--author">작가 : {{transaction.product.description}}</div>
+        <!-- <div class="product-txt--id">제품ID : {{ transaction.product.productId }}</div> -->
+        <div class="product-txt--title">{{ transaction.product.title }}</div>
+        <div class="product-txt--price">{{ transaction.product.price }}원</div>
+        <div class="product-txt--author">{{ transaction.product.description }}</div>
       </div>
     </div>
     <div class="detailId">
-      <div>{{transaction.detailId}}</div>
+      <div>{{ transaction.detailId }}</div>
     </div>
     <div class="timePaid">
-      <div>{{getDate(transaction.timePaid)}}</div>
+      <div>{{ getDate(transaction.timePaid) }}</div>
       <!-- <div>{{transaction.timePaid}}</div> -->
     </div>
     <div class="isCanceled">
       <!-- <div>{{!transaction.isCanceled ? (!transaction.done ? '거래요청': '거래완료') : '취소됨'}}</div> -->
-      <div>{{isCanceledComputed}}</div>
+      <div>{{ isCanceledComputed }}</div>
     </div>
   </div>
-
-  
 </template>
 
 <script>
-import { mapStores } from "pinia";
-import { useProductStore } from "~/store/product";
+import { mapStores } from 'pinia'
+import { useProductStore } from '~/store/product'
 import dayjs from 'dayjs'
 
 export default {
   props: {
     transaction: {
       type: Object,
-      require: true
-    }
+      require: true,
+    },
   },
   data() {
     return {
       inFocusTitle: false,
-      
     }
   },
   computed: {
     ...mapStores(useProductStore),
     isCanceledComputed() {
-      return !this.transaction.isCanceled ? (!this.transaction.done ? '거래요청': '거래완료') : '취소됨'
-    }
+      return !this.transaction.isCanceled
+        ? !this.transaction.done
+          ? '거래요청'
+          : '거래완료'
+        : '취소됨'
+    },
   },
   //  watch: {
   //   this.transaction.isCanceled,
@@ -64,57 +63,76 @@ export default {
   // },
   methods: {
     // getDate(time) {
-    //   // time 
+    //   // time
     //   return dayjs.utc(time).format('YY년 MM월 YY일 HH:mm')
     // }
     getDate(timestamp) {
       return dayjs(timestamp).format('MM월 YY일 HH:mm')
     },
-    
-  }
-
+  },
 }
-
 </script>
 
 <style lang="scss" scoped>
-
 .container {
-  background-color: aquamarine;
+  /* background-color: aquamarine; */
   display: grid;
-  grid-template-columns: [col-start] 50px [col1] 1fr [col1] 150px [col1] 150px [col1] 100px[col-end];
+  grid-template-columns: [col-start] 100px [col1] 1fr [col1] 150px [col1] 150px [col1] 100px[col-end];
   gap: 20px 10px;
   justify-content: center;
   align-items: center;
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid;
+  border-radius: 5px;
+
   &:hover {
-      transform:scale(1.02);
-      transition: transform .1s;
-      cursor: pointer;
-    }
+    transform: scale(1.01);
+    transition: transform 0.1s;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  }
+
   .product-img {
-      img{
-        width: 50px;
-        height: 50px;
-      }
-      .no-image{
-        width: 50px;
-        height: 50px;
-        margin: 10px auto;
-        background-image: url('~/static/no_image.jpg');
-        background-position: center;
-        background-size: cover;
-      }
+    img {
+      width: 100px;
+      height: 100px;
+      object-fit: cover;
     }
+    .no-image {
+      width: 50px;
+      height: 50px;
+      margin: 10px auto;
+      background-image: url('~/static/no_image.jpg');
+      background-position: center;
+      background-size: cover;
+    }
+  }
   .product {
     display: flex;
     flex-direction: column;
-    
-    
+
     &-txt {
       display: flex;
       flex-direction: column;
+      &--title {
+        font-size: 18px;
+        font-weight: bold;
+      }
+      &--price {
+        font-size: 17px;
+        font-weight: normal;
+        color: #7f7f7f;
+      }
+      &--author {
+        font-size: 15px;
+        font-weight: normal;
+        color: #7f7f7f;
+      }
     }
   }
+  .isCanceled {
+    font-weight: bold;
+  }
 }
-
 </style>
